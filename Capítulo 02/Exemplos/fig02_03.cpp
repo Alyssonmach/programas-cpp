439762a7a6adb94dd1f4a2ac30e89d3a9d03cc78
Alyssonmach <58775072+Alyssonmach@users.noreply.github.com>
2020-07-16 10:54:56 -0300

    Create fig02_03.cpp

---
+++ Capítulo 02/Exemplos/fig02_03.cpp
@@ -0,0 +1,15 @@
+// Figura 2.3: fig02_03.cpp
+// Autor: Alysson Machado
+// Horário: 10:25 AM
+// Imprimindo uma linha de texto com múltiplas instruções
+# include <iostream> // Permite ao programa realizar entrada e saída
+
+// A função main inicia a execução do programa
+int main()
+{
+    std::cout << "Welcome"; // Exibe a mensagem 
+	std::cout << " to C++!\n"; // Exibe a mensagem
+	
+    return 0; // Indica que o programa terminou com sucesso
+
+} // Fim da função main
