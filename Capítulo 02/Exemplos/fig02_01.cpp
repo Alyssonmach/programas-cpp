84a27945466177701ac0e11982f404f8c0d5bf25
Alyssonmach <58775072+Alyssonmach@users.noreply.github.com>
2020-07-16 09:31:11 -0300

    create fig02_01.cpp

---
+++ Capítulo 02/Exemplos/fig02_01.cpp
@@ -0,0 +1,12 @@
+// Figura 2.1: fig02_01.cpp
+// Programa de impressão de texto
+# include <iostream> // Permite que o programa gere saída de dados na tela
+
+// A função main inicia a execução do programa
+int main()
+{
+    std::cout << 'Welcome to C++!\n' // Exibe a mensagem
+
+    return 0 // Indica que o programa terminou com sucesso
+
+} // Fim da função main
