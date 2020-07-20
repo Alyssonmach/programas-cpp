a43e4731634c90a2ac0394e86ab806194e312053
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-20 16:37:00 -0300

    Create ex02_22.cpp

---
+++ Capítulo 02/Exercícios/ex02_22.cpp
@@ -0,0 +1,16 @@
+// Exercício 2.22: ex02_22.cpp
+// Autor: Alysson Machado
+// Data: 20/07/2020
+// Horário: 16:28 PM
+// O que o programa imprime?
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*\n**\n***\n****\n*****" << endl;
+	
+	return 0;
+}
