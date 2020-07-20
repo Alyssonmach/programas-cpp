53463e9a62ab39d3378300f3a145e3d22cf6a385
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-20 16:35:18 -0300

    Create ex02_17.cpp

---
+++ Capítulo 02/Exercícios/ex02_17.cpp
@@ -0,0 +1,22 @@
+// Exercício 2.17: ex02_17.cpp
+// Autor: Alysson Machado
+// Data: 20/07/2020
+// Horário: 16:02 PM
+// Imprime os números de 1 a 4 usando diferentes inserções de fluxo
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "1 2 3 4" << endl;
+	cout << 1 << " " << 2 << " " << 3 << " " << 4 << endl;
+	cout << 1 << " ";
+	cout << 2 << " ";
+	cout << 3 << " ";
+	cout << 4 << endl;
+	
+	return 0;
+}
