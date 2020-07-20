0316da84f831c720bf8880de0787bac752796907
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-20 16:35:48 -0300

    Create ex02_18.cpp

---
+++ Capítulo 02/Exercícios/ex02_18.cpp
@@ -0,0 +1,30 @@
+// Exercício 2.18: ex02_18.cpp
+// Data: 20/07/2020
+// Horário: 15:57 PM
+// Maior de dois números
+#include <iostream>
+#include <locale>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	int integer1;
+	int integer2;
+	
+	cout << "Enter two integers: ";
+	cin >> integer1 >> integer2;
+	
+	if(integer1 > integer2)
+		cout << integer1 << " é o maior." << endl;
+	else if (integer2 > integer1)
+		cout << integer2 << " é o maior." << endl;
+	else
+		cout << "Estes números são iguais." << endl;
+	
+	return 0;
+}
