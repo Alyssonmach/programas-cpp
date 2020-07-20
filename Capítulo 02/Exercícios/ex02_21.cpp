b301457ec145168a98638d7dc053edbaf22a34b2
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-20 16:36:42 -0300

    Create ex02_21.cpp

---
+++ Capítulo 02/Exercícios/ex02_21.cpp
@@ -0,0 +1,25 @@
+// Exercício 2.21: ex02_21.cpp
+// Autor: Alysson Machado
+// Data: 20/07/2020
+// Horário: 16:13 PM
+// Impressões Geométricas
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "*********     ***        *           *         " << endl;
+	cout << "*       *    *   *      **          * *        " << endl;
+	cout << "*       *   *     *    ****        *   *       " << endl;
+	cout << "*       *   *     *   ******      *     *      "<< endl;
+	cout << "*       *   *     *     *        *       *     " << endl;
+	cout << "*       *   *     *     *         *     *      " << endl;
+	cout << "*       *   *     *     *          *   *       " << endl;
+	cout << "*       *    *   *      *           * *        " << endl;
+	cout << "*********     ***       *            *         " << endl;
+	
+	return 0;
+}
