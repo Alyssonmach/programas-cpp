77b619394afd1ee8daba69513bf3bb1c438b022b
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-23 08:51:44 -0300

    Create ex02_23.cpp

---
+++ Capítulo 02/Exercícios/ex02_23.cpp
@@ -0,0 +1,27 @@
+// Exercício
+// Autor: Alysson Machado
+// Data: 20/07/2020
+// Horário: 16:39 PM
+// Maior e menor de cinco números inteiros
+#include <iostream>
+#include <locale>
+
+using std::cout;
+using std::cin;
+uisng std::endl;
+
+int main()
+{
+	
+	int int1, int2, int3, int4, int5;
+	int maior, menor;
+	
+	cout << "Enter five integers: ";
+	cin >> int1 >> int2 >> int3 >> int4 >> int5;
+	
+	if(int1 >= int2)
+		maior = int1;
+	else if ()
+
+	return 0;	
+}
