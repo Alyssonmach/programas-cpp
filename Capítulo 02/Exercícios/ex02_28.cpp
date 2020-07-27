7db66f566a95fc707781f0b8fd2e962f540b3f8f
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-27 16:54:11 -0300

    Create ex02_28.cpp

---
+++ Capítulo 02/Exercícios/ex02_28.cpp
@@ -0,0 +1,24 @@
+// Exercício 2.28: ex02_28.cpp
+// Autor: Alysson Machado
+// Data: 27/07/2020
+// Horário: 16:31 PM
+// Separa os digitos de um inteiro com cinco digitos
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	
+	int number;
+	cout << "Enter an integer with five digits: ";
+	cin >> number;
+	
+	cout << number / 10000 << " " << (number / 1000) % 10 << " ";
+	cout << (number / 100) % 10 << " " << (number / 10) % 10;
+	cout << " " << number % 10 << endl;
+	
+	return 0;
+}
