68f2a7963a40e81d30a325d4301fda09555a6bb7
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-29 11:22:33 -0300

    Update fig02_01.cpp

--- Capítulo 02/Exemplos/fig02_01.cpp
+++ Capítulo 02/Exemplos/fig02_01.cpp
@@ -1,7 +1,4 @@
 // Figura 2.1: fig02_01.cpp
-// Autor: Alysson Machado
-// Data: 16/07/2020
-// Horário: 09:45 AM
 // Programa de impressão de texto
 # include <iostream> // Permite ao programa realizar entrada e saída
 // A função main inicia a execução do programa
