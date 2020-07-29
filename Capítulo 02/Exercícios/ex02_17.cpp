a538ff7504464efe2634991fb2ece67a79e5006c
Alyssonmach <alyssonmachado388@gmail.com>
2020-07-29 11:23:02 -0300

    Update ex02_17.cpp

--- Capítulo 02/Exercícios/ex02_17.cpp
+++ Capítulo 02/Exercícios/ex02_17.cpp
@@ -1,7 +1,4 @@
 // Exercício 2.17: ex02_17.cpp
-// Autor: Alysson Machado
-// Data: 20/07/2020
-// Horário: 16:02 PM
 // Imprime os números de 1 a 4 usando diferentes inserções de fluxo
 #include <iostream>
 
