82d262bb44174944df3f853c9f4d42aa98d2126e
Alyssonmach <alyssonmachado388@gmail.com>
2020-09-02 18:01:54 -0300

    Create un01_ex02.cpp

---
+++ Unidade 01/Exemplos/un01_ex02.cpp
@@ -0,0 +1,20 @@
+// Imprimindo várias informações - un01_ex02.cpp
+
+// Biblioteca dos comandos padrão de entrada e saída
+#include <iostream>
+
+// Pacote std define o conjunto de funções que será usado no
+// programa, agrupando todas as bibliotecas de C++ pré-definidas
+using namespace std;
+
+// Declarando a função principal do programa
+int main()
+{
+	// inicializando as variáveis
+	int val = 10;
+	
+	cout << "Valor = " << val << endl; // Imprimindo na saída de dados
+	
+	// Indica que o programa terminou com sucesso
+	return 0;
+} // Fim da função principal
