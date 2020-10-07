95f07f5850cb8a2f7ff29c3eb98f488f462828ac
Alyssonmach <alyssonmachado388@gmail.com>
2020-10-07 20:03:55 -0300

    Update Funcionario.cpp

--- Unidade 05/Laboratórios/Laboratório 14/Funcionario.cpp
+++ Unidade 05/Laboratórios/Laboratório 14/Funcionario.cpp
@@ -1,25 +1,69 @@
+// Implementando um programa que simula uma empresa - Funcionario.cpp
+
+// Aluno Alysson Machado de Oliveira Barbosa
+// Matrícula: 119110236
+// Técnicas de Programação (C++) - Salerno
+// Unidade 05 - Laboratório 14 
+
 #include "Funcionario.h"
-#include <string>
 
-using std::string;
-
+// Implementando o construtor da classe Funcionario
 Funcionario::Funcionario(void)
 {
-	nome = "Nome Desconhecido";
-	salario = 0.0;
-	dia = mes = ano = 0;
+	this -> data.dia = 0;
+	this -> data.mes = 0;
+	this -> data.ano = 0;
+	this -> nome = "";
+ 	this -> salario = 0.0;
+ 	this -> departamento = "";
 }
 
-Funcionario::Funcionario(string n, float s, int d, int m, int a)
+// Método para configurar o nome do funcionário
+void Funcionario::setNome(string nome)
 {
-	nome = n;
-	salario = s;
-	dia = d;
-	mes = m;
-	ano = a;
+	this -> nome = nome;
 }
 
-Funcionario::~Funcionario(void)
+// Método para configurar o salário do funcionário
+void Funcionario::setSalario(float salario)
 {
-	delete
+	this -> salario = salario;
+}
+
+// Método para configurar a data de admissão do funcionário
+void Funcionario::setData(int dia, int mes, int ano)
+{
+	this -> data.dia = dia;
+	this -> data.mes = mes;
+	this -> data.ano = ano;
+}
+
+// Método para configurar o departamento do funcionário
+void Funcionario::setDepartamento(string departamento)
+{
+	this -> departamento = departamento;
+}
+
+// Método para obter o nome do funcionário
+string Funcionario::getNome(void) const
+{
+	return nome;
+}
+
+// Método para obter o salário do funcionário
+float Funcionario::getSalario(void) const
+{
+	return salario;
+}
+
+// Método para obter a data de admissão do funcionário
+DataAdmissao Funcionario::getData(void) const
+{
+	return data;
+}
+
+// Método para obter o departamento do funcionário
+string Funcionario::getDepartamento(void) const
+{
+	return departamento;
 }
