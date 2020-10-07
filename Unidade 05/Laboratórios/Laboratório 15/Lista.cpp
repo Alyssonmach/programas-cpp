2a3d0aef1a4d383b71a6867cf86d0a8953ae8e2e
Alyssonmach <alyssonmachado388@gmail.com>
2020-10-07 20:52:33 -0300

    Update Lista.cpp

--- Unidade 05/Laboratórios/Laboratório 15/Lista.cpp
+++ Unidade 05/Laboratórios/Laboratório 15/Lista.cpp
@@ -1,5 +1,13 @@
+// Implementando um programa Cria e Ordena uma lista de nomes - Lista.cpp
+
+// Aluno Alysson Machado de Oliveira Barbosa
+// Matrícula: 119110236
+// Técnicas de Programação (C++) - Salerno
+// Unidade 05 - Laboratório 15
+
 #include "Lista.h"
 
+// Implementado o construtor da classe
 Lista::Lista(int tam)
 {
 	maxLista = (tam > 0) ? tam : 0;
@@ -7,6 +15,7 @@ Lista::Lista(int tam)
 	total = 0;
 }
 
+// Configurando a lista de nomes
 void Lista::setLista(NomesFuncionarios nomes)
 {
 	if(total < maxLista)
@@ -16,12 +25,13 @@ void Lista::setLista(NomesFuncionarios nomes)
 	}
 }
 
+// Ordenando a lista de nomes em ordem alfabética (Ordenação Bolha + métodos da classe string)
 void Lista::OrdenaAlfabetica(void)
 {
 	string aux;
 	
-	for(int i = 0; i < total; i++)
-		for(int j = 0; j < total - 1; j++)
+	for(int i(1); i < total; i++)
+		for(int j(0); j < total - i; j++)
 			if(ptr[j].nome.compare(ptr[j + 1].nome) == 1)
 			{
 				aux = ptr[j].nome;
@@ -30,6 +40,7 @@ void Lista::OrdenaAlfabetica(void)
 			}
 }
 
+// Método responsável por imprimir a lista de nomes para o usuário
 void Lista::imprimeLista(void)
 {
 	for(int i = 0; i < total; i++)
@@ -38,8 +49,9 @@ void Lista::imprimeLista(void)
 	}
 }
 
+// Definindo um destrutor para a classe
 Lista::~Lista(void)
 {
 	delete [] ptr;
-	cout << "A lista foi destruida." << endl;
+	cout << "O cache da lista foi removido." << endl;
 }
