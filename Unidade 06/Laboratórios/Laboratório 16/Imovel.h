e9fdd774d7f94452faddf754860e7f4507837848
Alyssonmach <alyssonmachado388@gmail.com>
2020-10-20 23:11:54 -0300

    Update Imovel.h

--- Unidade 06/Laboratórios/Laboratório 16/Imovel.h
+++ Unidade 06/Laboratórios/Laboratório 16/Imovel.h
@@ -1,6 +1,15 @@
+// Implementação de um programa que simula o gerenciamento de imóveis - Imovel.h
+
+// Aluno Alysson Machado de Oliveira Barbosa
+// Matrícula: 119110236
+// Técnicas de Programação (C++) - Salerno
+// Unidade 06 - Laboratório 16 
+// Ambiente de Desenvolvimento: Falcon C++
+
 #ifndef IMOVEL_H
 #define IMOVEL_H
 
+// definindo uma struct para armazenar o endereço do cliente
 struct Endereco
 {
 	string rua;
@@ -9,8 +18,10 @@ struct Endereco
 	string CEP;
 };
 
+// definindo uma classe base Imovel 
 class Imovel 
 {
+	// Especificação de acesso protected para dar acesso a outras classes 
 	protected:
 		Endereco endereco;
 		float preco;
