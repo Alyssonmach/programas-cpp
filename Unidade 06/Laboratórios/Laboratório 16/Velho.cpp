f18f5584fde89fdc3c9720aee1ee82467f981394
Alyssonmach <alyssonmachado388@gmail.com>
2020-10-20 23:12:04 -0300

    Update Velho.cpp

--- Unidade 06/Laboratórios/Laboratório 16/Velho.cpp
+++ Unidade 06/Laboratórios/Laboratório 16/Velho.cpp
@@ -1,5 +1,14 @@
+// Implementação de um programa que simula o gerenciamento de imóveis - Velho.cpp
+
+// Aluno Alysson Machado de Oliveira Barbosa
+// Matrícula: 119110236
+// Técnicas de Programação (C++) - Salerno
+// Unidade 06 - Laboratório 16 
+// Ambiente de Desenvolvimento: Falcon C++
+
 #include "Velho.h"
 
+// Definindo o construtor da classe Velho
 Velho::Velho(string rua, int numero, string bairro, string cep, float preco) : Imovel(rua, numero, bairro, cep, preco)
 {
 	this -> endereco.rua = rua;
@@ -9,11 +18,13 @@ Velho::Velho(string rua, int numero, string bairro, string cep, float preco) : I
 	this -> preco = preco;
 }
 
+// Método da classe que configura o preço da casa velha
 void Velho::setPreco(float preco)
 {
 	this -> preco = (preco > 0) ? preco:(this -> preco);
 }
 
+// Método da classe que configura o endereço da casa velha
 void Velho::setEndereco(string rua, int numero, string bairro, string cep)
 {
 	this -> endereco.rua = rua;
@@ -23,16 +34,19 @@ void Velho::setEndereco(string rua, int numero, string bairro, string cep)
 	
 }
 
+// Método da classe que retorna o preço sem a taxa de desconto da casa velha
 float Velho::getPreco(void) const
 {
 	return preco;
 }
 
+// Método da classe que retorna o preço com a taxa de desconto da casa velha
 float Velho::getPrecoDesconto(void) const
 {
 	return preco - (preco * getDecrescenta());
 }
 
+// Método da classe que retorna o endereço da casa velha
 void Velho::getEndereco(void) const
 {
 	cout << "Rua: " << endereco.rua << endl;
@@ -41,4 +55,5 @@ void Velho::getEndereco(void) const
 	cout << "CEP: " << endereco.CEP << endl;
 }
 
+// Inicializando a porcentagem de desconto (constante)
 float Velho::porcentagem_decrescimo = 10;
