b83e50f1cbbd7b801999a76729bff6b3f35879db
Alyssonmach <alyssonmachado388@gmail.com>
2020-10-20 23:11:51 -0300

    Update Imovel.cpp

--- Unidade 06/Laboratórios/Laboratório 16/Imovel.cpp
+++ Unidade 06/Laboratórios/Laboratório 16/Imovel.cpp
@@ -1,5 +1,14 @@
+// Implementação de um programa que simula o gerenciamento de imóveis - Imovel.cpp
+
+// Aluno Alysson Machado de Oliveira Barbosa
+// Matrícula: 119110236
+// Técnicas de Programação (C++) - Salerno
+// Unidade 06 - Laboratório 16 
+// Ambiente de Desenvolvimento: Falcon C++
+
 #include "Imovel.h"
 
+// definindo o construtor da classe Imovel
 Imovel::Imovel(string rua, int numero, string bairro, string cep, float preco)
 {
 	this -> endereco.rua = rua;
